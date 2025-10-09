ff311cb5926a3e6765262845946536f1a2bdcb77
Anton Yaroshenko <kpakadijl@hotmail.com>
2025-10-09 22:43:54 +0300

    Refactor Forbidden case in error handling logic
    Separated HttpStatusCode.Forbidden from other status codes
    and implemented custom error-handling logic. Instead of
    deserializing the response into a LokoErrorResponse, the
    raw response content is now read as a string and used to
    create a new LokoErrorResponse with a detailed error
    message, HTTP status code, and the raw message in the
    Errors list. This ensures more specific and informative
    error handling for forbidden access scenarios.

--- FourTwenty.LokoMerchant.Client/FourTwenty.LokoMerchant.Client/Helpers/ErrorHandlingHelper.cs
+++ FourTwenty.LokoMerchant.Client/FourTwenty.LokoMerchant.Client/Helpers/ErrorHandlingHelper.cs
@@ -8,10 +8,18 @@
             {
                 case HttpStatusCode.BadRequest:
                 case HttpStatusCode.Unauthorized:
-                case HttpStatusCode.Forbidden:
                 case HttpStatusCode.NotFound:
                     var error = await response.Content.ReadFromJsonAsync<LokoErrorResponse>(ct);
                     throw new LokoException(error);
+                case HttpStatusCode.Forbidden:
+                    var rawMessage = await response.Content.ReadAsStringAsync(ct);
+                    throw new LokoException(new LokoErrorResponse()
+                    {
+                        Message = "Access forbidden. You do not have permission to access this resource.",
+                        Code = (int)HttpStatusCode.Forbidden,
+                        Errors =
+                        [rawMessage]
+                    });
                 default:
                     response.EnsureSuccessStatusCode();
                     return;
